cc7ffb6b283cc2957e5b53163c03e04f57591517
Vegapeanut01 <rodrigodecastrohenrique@hotmail.com>
2023-03-15 17:01:41 -0300

    Algoritmo de busca binária em Python

---
+++ BuscaBinaria.py
@@ -0,0 +1,28 @@
+#Algoritmo de busca binária
+def executar_busca_binaria(lista, valor):
+    minimo = 0
+    maximo = len(lista) - 1
+    while minimo <= maximo:
+        meio = (minimo + maximo) // 2
+        if valor < lista[meio]:
+            maximo = meio - 1
+        elif valor > lista[meio]:
+            minimo = meio + 1
+        else:
+            return True
+    return False
+
+
+#Retorando a posição do valor na lista
+def procurar_valor(lista, valor):
+    minimo = 0
+    maximo = len(lista) - 1
+    while minimo <= maximo:
+        meio = (minimo + maximo) // 2
+        if valor < lista[meio]:
+            maximo = meio - 1
+        elif valor > lista[meio]:
+            minimo = meio + 1
+        else:
+            return meio
+    return None
